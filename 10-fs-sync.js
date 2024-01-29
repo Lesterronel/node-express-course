1f416740c782f7ef2fcd882d24e148de1f64cf81
Ronel Lester Torregoza <ronel.torregoza@ionics.local>
2024-01-29 13:39:01 +0800

    Added new files.January 29, 2024

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -1,6 +1,13 @@
 const {readFileSync,writeFileSync} = require('fs')
-
+console.log('start');
 const first = readFileSync('./content/first.txt','UTF8')
 const second = readFileSync('./content/second.txt','UTF8')
 
-writeFileSync('./content/result-sync.txt',`Here is the result : ${first}, ${second}`, {flag : 'a'})
+writeFileSync(
+    './content/result-sync.txt',
+    `Here is the result : ${first}, ${second}`, 
+    {flag : 'a'}
+)
+
+console.log('done with the task');
+console.log('starting the next one');
